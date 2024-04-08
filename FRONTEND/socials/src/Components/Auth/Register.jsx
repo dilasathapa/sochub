5db0f2042318666db51cae19b995be6645a93a5c
dilasathapa <thapadilasa71@gmail.com>
2024-04-08 22:25:39 +0530

    registration logic done

--- FRONTEND/socials/src/Components/Auth/Register.jsx
+++ FRONTEND/socials/src/Components/Auth/Register.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import {useForm} from "react-hook-form"
 import Navbar from "../../Routes/Navbar";
 import registerpic from "./../../assets/illustrations/registrationpic.svg"
 import styles from "../../Styles/Register.module.css"
 import logo from "../../assets/react.svg"
 
+
 const Register = () => {
+
+    const {register, handleSubmit, formState : {errors, isSubmitSuccessful, isSubmitted}} = useForm();
+    
+    function submitData(data){
+        console.log(data)
+    }
+
     return (
         <>
             <div>
@@ -17,16 +26,28 @@ const Register = () => {
                     <div>
                         <div><img src={logo} alt="logo" /></div>
                         <div className={styles.form_container}>
-                            <form>
+                            <form onSubmit={handleSubmit(submitData)}>
                                 <label htmlFor="user_emailID">emailID or username</label> 
-                                <input type="text" id="user_emailID" placeholder="enter emailID" />
+                                <input type="text" id="user_emailID" placeholder="enter emailID" 
+                                {
+                                    ...register("user_emailID", {
+                                        required : "your emailID is reuired"
+                                    })
+                                }
+                                />
 
                                 <label htmlFor="user_password">password</label>
-                                <input type="password" id="user_password" placeholder="enter your password" />
+                                <input type="password" id="user_password" placeholder="enter your password" 
+                                {
+                                    ...register("user_password", {
+                                        required : "your password is required"
+                                    })
+                                }
+                                />
 
 
 
-                                <button className={styles.register_button}>Register</button>
+                                <button type="submit" className={styles.register_button}>Register</button>
                             </form>
                             <hr />
                             <button className={styles.github_button}>signup with github</button>

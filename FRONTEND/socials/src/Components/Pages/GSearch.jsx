d9520fb8be87fa43254f8d54783720af88193067
dilasathapa <thapadilasa71@gmail.com>
2024-05-01 19:24:25 +0530

    Revert "added profilecards"
    This reverts commit dff222f498a315a83c39489a038235fe00ae2954.

--- FRONTEND/socials/src/Components/Pages/GSearch.jsx
+++ FRONTEND/socials/src/Components/Pages/GSearch.jsx
@@ -1,54 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../../Routes/Navbar";
 import NosearchPage from "./NosearchPage";
-import styles from "../../Styles/GSearch.module.css"
-import SearchedUserProfile from "./SearchedUserProfile";
 
 
-const GSearch = () => {
-    const [inputValue, setInputValue] = useState("")
-    console.log(inputValue);
-
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-    }
+const GSearch =()=>{
     return (
         <>
             <Navbar />
-            <div className={styles.search_parent_container}>
-                <div >
-
-                    <input type="text"
-                        placeholder="search for github users..."
-                        // className={styles`.${search_field}`} 
-                        className={`${styles.search_field} ${inputValue ? styles.moved : ''}`}
-
-                        onChange={handleInputChange}
-                    />
-
-                </div>
-                <br />
-                <div className={styles.user_details}>
-
-                    {
-                        inputValue.length > 0 ? (
-                            // <h1>hello</h1>
-                            <div className={styles.users_field}>
-                                <SearchedUserProfile />
-                                <SearchedUserProfile />
-                                <SearchedUserProfile />
-
-                                {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus atque nisi assumenda libero excepturi est, temporibus minima eaque magnam, a ab laborum labore voluptatibus maiores esse dolorem, fugit rerum impedit.</p> */}
-
-                            </div>
-                        ) : (
-
-                            <NosearchPage />
-                        )
-                    }
-                </div>
-            </div>
-
+            <h1>Gsearsdfvsfch</h1>
+            <NosearchPage />
         </>
     )
 }

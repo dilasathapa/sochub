9a2c10b58a83ba2a5ad717aa61cd033317113dd0
dilasathapa <thapadilasa71@gmail.com>
2024-05-13 14:40:16 +0530

    improved git search page

--- FRONTEND/socials/src/Components/Pages/GSearch.jsx
+++ FRONTEND/socials/src/Components/Pages/GSearch.jsx
@@ -1,46 +1,80 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../Routes/Navbar";
 import NosearchPage from "./NosearchPage";
-import styles from "../../Styles/GSearch.module.css"
+import styled_GS from "../../Styles/GSearch.module.css"
 import SearchedUserProfile from "./SearchedUserProfile";
 
 
 const GSearch = () => {
-    const [inputValue, setInputValue] = useState("")
-    console.log(inputValue);
+    const [searchTerm, setSearchTerm] = useState("")
+    const [searchHistory, setSearchHistory] = useState([])
+    const [users, setUsers] = useState([])
 
+    console.log(import.meta.env.VITE_APP_GITHUB_ACCESS_TOKEN)
+    console.log("users", users)
     const handleInputChange = (e) => {
-        setInputValue(e.target.value);
+        setSearchTerm(e.target.value);
     }
+
+    // using debouncing
+    useEffect(() => {
+        const debounceTimer = setTimeout(() => {
+            if (searchTerm.length != 0) {
+                setSearchHistory((prevHistory) => [
+                    ...prevHistory, searchTerm
+                ])
+            }
+        }, 500)
+        return () => clearTimeout(debounceTimer)
+    }, [searchTerm])
+
+    //searching the user from github api
+    useEffect(() => {
+        async function getData() {
+            if (searchHistory[searchHistory.length - 1] != undefined && searchHistory[searchHistory.length - 1] != "") {
+                const apiUrl = `https://api.github.com/users/${searchHistory[searchHistory.length - 1]}`;
+                fetch(apiUrl, {
+                    headers: {
+                        Authorization: `Token ${import.meta.env.VITE_APP_GITHUB_ACCESS_TOKEN}`
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => setUsers((prevUserData) => [...prevUserData, data]))
+                    .catch(error => console.log(error))
+            }
+        }
+        getData()
+    }, [searchHistory])
+
+
+
     return (
         <>
             <Navbar />
-            <div className={styles.search_parent_container}>
+            <div className={styled_GS.search_parent_container}>
                 <div >
 
                     <input type="text"
                         placeholder="search for github users..."
-                        // className={styles`.${search_field}`} 
-                        className={`${styles.search_field} ${inputValue ? styles.moved : ''}`}
+                        // className={styled_GS`.${search_field}`} 
+                        className={`${styled_GS.search_field} ${searchTerm ? styled_GS.moved : ''}`}
 
                         onChange={handleInputChange}
                     />
 
                 </div>
                 <br />
-                <div className={styles.user_details}>
+                <div className={styled_GS.users}>
 
                     {
-                        inputValue.length > 0 ? (
+                        searchTerm.length > 0 ? (
                             // <h1>hello</h1>
-                            <div className={styles.users_field}>
-                                <SearchedUserProfile />
-                                <SearchedUserProfile />
-                                <SearchedUserProfile />
 
-                                {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus atque nisi assumenda libero excepturi est, temporibus minima eaque magnam, a ab laborum labore voluptatibus maiores esse dolorem, fugit rerum impedit.</p> */}
-
-                            </div>
+                            <div className={styled_GS.users_field}>
+                            {users.map((user, i) => (
+                                    <SearchedUserProfile user={user} key={i} />
+                            ))}
+                                </div>
                         ) : (
 
                             <NosearchPage />
--- FRONTEND/socials/src/Components/Pages/SearchedUserProfile.jsx
+++ FRONTEND/socials/src/Components/Pages/SearchedUserProfile.jsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { BsGithub } from "react-icons/bs";
+import { BsTwitterX } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import styled from "../../Styles/SearchedUserprofile.module.css"
 
-import styles from "../../Styles/SearchedUserprofile.module.css"
-
-const SearchedUserProfile = () => {
+const SearchedUserProfile = ({user}) => {
     return (
         <>
             {/* <div className={styles.rough}> */}
 
-            <div className={styles.parent_container}>
-                <div className={styles.image_container}>
-                    <img src="https://preview.redd.it/how-strong-is-jiraiya-v0-0hdtt6zrqycb1.jpg?width=450&format=pjpg&auto=webp&s=2a4969be966363c03b43dd59788f110d3929f6ca" alt="sdfv" />
+            <div className={styled.parent_container}>
+                <div className={styled.image_container}>
+                    <img src={user.avatar_url} alt="sdfv" />
                 </div>
-                <div className={styles.user_details}>
-                    <h3>jiraiya sama</h3>
-                    <h4>erro sennin</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptatem et nihil veritatis pariatur molestias quidem magnam, cum nobis, ipsam eligendi ex alias nemo cupiditate. Saepe consectetur temporibus alias vero!</p>
+                <div className={styled.user_details}>
+                    <h3>{user.login}</h3>
+                    <h4>{user.name}</h4>
+                    <p>{user.bio}</p>
                 </div>
-                <div className={styles.links}>
+                <div className={styled.links}>
                     <div>
                         <p><BsGithub /></p>
                     </div>
                     <div>
-                        <p><BsGithub /></p>
-                        <p><BsGithub /></p>
+                        <p><BsTwitterX /></p>
+                        <p><BsLinkedin /></p>
                     </div>
                 </div>
             </div>

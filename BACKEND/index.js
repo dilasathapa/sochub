3ac5a8ab6f218440638e9a8a115e99e785aa6cab
dilasathapa <thapadilasa71@gmail.com>
2024-07-26 21:47:00 +0530

    setting the limit

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -9,8 +9,9 @@ const userRouter = require('./routes/getusers.routes');
 const app = express();
 app.use(cors());
 // app.use(express.json())
-app.use(bodyParser.json());
-app.use(express.json({limit: '10mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.json({limit: '50mb'}));
+// app.use(express.bodyParser({limit: '50mb'}));
 
 
 
